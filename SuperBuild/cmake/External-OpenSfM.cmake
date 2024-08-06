20df789c69070a1ede29057868ce75500dbd0192
SeunghoBack <seungho.back@nearthlab.com>
2024-08-06 20:01:01 +0900

    local repo to nearthlab repo

--- SuperBuild/cmake/External-OpenSfM.cmake
+++ SuperBuild/cmake/External-OpenSfM.cmake
@@ -24,7 +24,7 @@ ExternalProject_Add(${_proj_name}
   STAMP_DIR         ${_SB_BINARY_DIR}/stamp
   #--Download step--------------
   DOWNLOAD_DIR      ${SB_DOWNLOAD_DIR}
-  GIT_REPOSITORY    https://github.com/SeunghoBack/OpenSfM/
+  GIT_REPOSITORY    https://github.com/nearthlab/OpenSfM/
   GIT_TAG           origin/7777
   #--Update/Patch step----------
   UPDATE_COMMAND    git submodule update --init --recursive
